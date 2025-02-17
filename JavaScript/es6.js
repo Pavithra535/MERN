f47decf8fd8a90a528697e89c837fe0cd46d9745
Pavithra535 <pavithramk535@gmail.com>
2025-02-17 13:25:45 +0530

    Day 7 FN

---
+++ JavaScript/es6.js
@@ -0,0 +1,51 @@
+//This is function
+/*function testFunction(){
+    console.log("Test Function");
+}
+testFunction();
+//This is Arrow function
+var arrowFunction = () =>{
+   console.log("This is arrow function");
+};
+arrowFunction();*/
+//3.Scoping
+//if(true)
+//4.Spread Operator
+/*stud1 = ["user1","user2","user3","user4"];
+stud2 = ["user5", "user6", "user7","user8"];
+stud3 = ["user9", "user10", "user11", "user12"];
+stud4 = [];
+studall = [...stud1,...stud2,...stud3,...stud4];
+console.log(studall);*/
+//5.Destructing Operator
+/*array = [10,30,50,80,100];
+var [a,b,c,d] = array;
+console.log(a,d);
+
+//rest operator
+function print(...number)
+{
+    console.log(number);
+}
+print(1,2,3,4);
+*/
+/*console.log(a);
+const a = 10;*/
+/*funHoisting();
+/*function funHoisting(){
+    console.log("This is function");
+}*/
+/*funHoisting = () => {
+    console.log("This is Arrow Function");
+}*/
+class Hello{
+    classfn(){
+       console.log("helo");
+       return 1;
+       
+    }
+}
+obj = new Hello();
+console.log(obj.classfn());
+
+ 
