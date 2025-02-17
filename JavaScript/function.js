6b8da108bdf7242eb6401d8bfea5e93fa2ffdee7
Pavithra535 <pavithramk535@gmail.com>
2025-02-17 08:31:47 +0530

    Day 6 task

---
+++ JavaScript/function.js
@@ -0,0 +1,19 @@
+function print(){
+    return "hello";
+}
+console.log(print());
+
+function add(a,b){
+    console.log(a+b);
+}
+add(10,15);
+
+function multiply(c,d){
+    return c*d;
+}
+console.log(multiply(5,2));
+
+function greet(){
+    console.log("Good Morning");
+}
+greet();
